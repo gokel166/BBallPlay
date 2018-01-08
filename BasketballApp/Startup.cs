968932f7363be9dacb8844859bb3179d63179e15
George Okello <okellogeorge40@yahoo.com>
2018-01-08 15:07:41 -0600

    Add new service

--- BasketballApp/Startup.cs
+++ BasketballApp/Startup.cs
@@ -26,7 +26,7 @@ namespace BasketballApp
             services.AddMvc();
 
             services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
