f8aa2cd24546892c1d1e0f687a1aa8f7da491bd5
George Okello <okellogeorge40@yahoo.com>
2018-01-09 00:35:47 -0600

    Add new service

---
+++ BasketballData/BasketballContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasketballData
+{
+    public class BasketballContext : DbContext
+    {
+        public BasketballContext(DbContextOptions options) : base(options){ }
+
+        public DbSet<Player> MyProperty { get; set; }
+    }
+}
