a36a43abbfe0988a21f1336241f1cc91a275b8bf
George Okello <okellogeorge40@yahoo.com>
2018-01-10 03:13:44 -0600

    Add Stat Class table before migration

---
+++ BasketballData/Models/Stat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasketballData.Models
+{
+    public class Stat
+    {
+        //Numeric stat line
+        public int Id { get; set; }
+        public int OREB { get; set; }
+        public int REB { get; set; }
+        public int AST { get; set; }
+        public int STL { get; set; }
+        public int BLK { get; set; }
+        public int TO { get; set; }
+        public int PF { get; set; }
+        public int PTS { get; set; }
+        public int FGA { get; set; }
+    }
+}
