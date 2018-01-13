abe9f52e524926c6c35b4873ce5082b8b6984d41
George Okello <okellogeorge40@yahoo.com>
2018-01-12 22:14:45 -0600

    adding new models

--- BasketballData/Models/CoachAwards.cs
+++ BasketballData/Models/CoachAwards.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace BasketballData.Models
 {
-    public class CoachAwards
+    public class CoachAwards : Coahes
     {
         public int Id { get; set; }
         public string CoachOfTheYear { get; set; }
